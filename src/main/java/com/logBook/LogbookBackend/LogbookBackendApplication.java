59d0a25f2e678515538898a4bd4871ef066e91eb
Collins C. A <contactmrc4@gmail.com>
2024-01-18 14:40:14 +0100

    first commit

---
+++ src/main/java/com/logBook/LogbookBackend/LogbookBackendApplication.java
@@ -0,0 +1,14 @@
+package com.logBook.LogbookBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbookBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LogbookBackendApplication.class, args);
+	}
+
+}
