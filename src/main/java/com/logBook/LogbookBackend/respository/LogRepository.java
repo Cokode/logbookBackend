ca006fa13a79fc8e04be56af879ac6b0b61bbf9b
Collins C. A <contactmrc4@gmail.com>
2024-02-13 23:28:30 +0100

    changed repository interface type to User

--- src/main/java/com/logBook/LogbookBackend/respository/LogRepository.java
+++ src/main/java/com/logBook/LogbookBackend/respository/LogRepository.java
@@ -1,11 +1,12 @@
   package com.logBook.LogbookBackend.respository;
 
   import com.logBook.LogbookBackend.model.Log;
+  import com.logBook.LogbookBackend.model.User;
   import org.springframework.data.repository.CrudRepository;
   import org.springframework.stereotype.Repository;
 
   @Repository
-  public interface LogRepository extends CrudRepository<Log, Long> {
+  public interface LogRepository extends CrudRepository<User, Long> {
     @Override
-    Iterable<Log> findAllById(Iterable<Long> longs);
+    Iterable<User> findAllById(Iterable<Long> longs);
   }
