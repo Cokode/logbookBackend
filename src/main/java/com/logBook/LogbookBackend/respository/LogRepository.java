7e087b9a68e40097ef5d81b28e0b2f78e99dd4a4
Collins C. A <contactmrc4@gmail.com>
2024-02-14 23:37:37 +0100

    class name 'User' renamed to 'LogUser'

--- src/main/java/com/logBook/LogbookBackend/respository/LogRepository.java
+++ src/main/java/com/logBook/LogbookBackend/respository/LogRepository.java
@@ -1,9 +1,8 @@
 package com.logBook.LogbookBackend.respository;
 
 import com.logBook.LogbookBackend.model.Log;
-import com.logBook.LogbookBackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
-  Log findLogByUser (User user);
+//  Log findLogByUser (LogUser user);
 }
