a832268427623f3724df9de7f62a7e7caf195181
Collins C. A <contactmrc4@gmail.com>
2024-02-18 13:41:59 +0100

    updated 'createUser' method, Login, and info verification Endpoint

--- src/main/java/com/logBook/LogbookBackend/controller/UserController.java
+++ src/main/java/com/logBook/LogbookBackend/controller/UserController.java
@@ -2,8 +2,17 @@ package com.logBook.LogbookBackend.controller;
 
 import com.logBook.LogbookBackend.BusinessLogic.UserService;
 import com.logBook.LogbookBackend.model.LogUser;
+import com.logBook.LogbookBackend.model.LoginDetails;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -19,8 +28,39 @@ public class UserController {
   }
 
   @PostMapping("/createUser")
-  public LogUser createUser(@RequestBody LogUser logUser) {
-   return  userService.createUser(logUser);
+  public LogUser createUser(@RequestBody LogUser logUser) throws Exception {
+    if (userService.createUser(logUser) == null) {
+      throw new ResponseStatusException(HttpStatusCode.valueOf(400));
+    }
+    throw new ResponseStatusException(HttpStatusCode.valueOf(201));
   }
 
+  @GetMapping("/getall")
+  public List<LogUser> allOf() {
+    return userService.getAll();
+  }
+
+  @GetMapping("/verifyUserName")
+  public ResponseEntity<HttpStatus> verifyUserName (@RequestParam("userName") String userName) {
+    if(!userService.verifyUserName(userName)) {
+      System.out.println("user found");
+     return ResponseEntity.badRequest().build();
+    }
+
+    System.out.println("user not found. I was here ERROR!");
+    return ResponseEntity.ok().build();
+  }
+
+  // TODO ignore case in email also, remove password
+      // field before sending object to client side.
+  @PostMapping("/verifyLogin")
+  public Optional<LogUser> verifyLogin (@RequestBody @NotNull LoginDetails loginInfo) {
+
+    if (userService.verifyUserLogin(loginInfo)) {
+      System.out.println(ResponseEntity.ok().build());
+      return Optional.ofNullable(userService.getUser(loginInfo));
+    }
+
+    return Optional.ofNullable(userService.getUser(loginInfo));
+  }
 }
