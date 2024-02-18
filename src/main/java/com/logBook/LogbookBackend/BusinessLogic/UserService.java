8cd603721e748b8e3d396eebcf4edc9f8afeec9f
Collins C. A <contactmrc4@gmail.com>
2024-02-18 13:41:01 +0100

    updated 'createUser' method, created Login, and infor verification Endpoint

--- src/main/java/com/logBook/LogbookBackend/BusinessLogic/UserService.java
+++ src/main/java/com/logBook/LogbookBackend/BusinessLogic/UserService.java
@@ -1,12 +1,18 @@
 package com.logBook.LogbookBackend.BusinessLogic;
 
+import com.logBook.LogbookBackend.model.Log;
+import com.logBook.LogbookBackend.model.LogType;
 import com.logBook.LogbookBackend.model.LogUser;
+import com.logBook.LogbookBackend.model.LoginDetails;
 import com.logBook.LogbookBackend.respository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -17,31 +23,108 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public LogUser createUser (@NotNull LogUser newLogUser) {
-    LogUser logUser;
-    if (userRepository.findUserByUserName(
-            newLogUser.getUserName()) == null
-      && userRepository.findUserByEmail(newLogUser.getEmail()) == null) {
+  public LogUser createUser (@NotNull LogUser newLogUser) throws Exception {
+   if (verifyUserDoesNotExist(newLogUser)) return null;
+   LogUser logUser = collectPayLoadInformation(newLogUser);
 
-      logUser = newLogUser;
-      userRepository.save(logUser);
-      return logUser;
+   if (logUser != null) {
+     boolean bol = verifyPayLoadInformation(logUser);
+     if(bol) userRepository.save(logUser);
+     return logUser;
+   }
+
+   return null;
+  }
+
+  public boolean verifyUserDoesNotExist (LogUser logUser) {
+    return userRepository.findAll().contains(logUser);
+  }
+
+  public LogUser collectPayLoadInformation(@NotNull LogUser newLogUser) {
+
+    if (!(verifyEmail(newLogUser.getEmail())
+      && verifyUserName(newLogUser.getUserName())
+      && verifyPhoneNumber(newLogUser.getPhoneNumber()))) {
+      return null;
     }
-    return null;
+
+    LogUser logUser = new LogUser();
+
+    logUser.setUserName(newLogUser.getUserName());
+    logUser.setFirstName(newLogUser.getFirstName());
+    logUser.setMiddleName(newLogUser.getMiddleName());
+    logUser.setLastName(newLogUser.getLastName());
+    logUser.setEmail(newLogUser.getEmail());
+    logUser.setPhoneNumber(newLogUser.getPhoneNumber());
+    logUser.setPassword(newLogUser.getPassword());
+    logUser.setDateOfBirth(newLogUser.getDateOfBirth());
+
+    System.out.println(logUser + " logUser after collecting information.");
+
+    return logUser;
+  }
+
+  public boolean verifyPayLoadInformation(LogUser newLogUser) throws Exception{
+    return newLogUser.getUserName() != null &&
+            newLogUser.getFirstName() != null &&
+            newLogUser.getLastName() != null &&
+            newLogUser.getEmail() != null &&
+            newLogUser.getPassword() != null &&
+            newLogUser.getDateOfBirth() != null &&
+            newLogUser.getPhoneNumber() != null;
+  }
+
+  public LogUser getUser(LoginDetails loginDetails) {
+    LogUser logUser = null;
+
+    if (verifyUserLogin(loginDetails)) {
+      logUser = userRepository.findUserByEmail(loginDetails.email()) != null ?
+        userRepository.findUserByEmail(loginDetails.email()) :
+          userRepository.findUserByPhoneNumber(loginDetails.email());
+    }
+    return logUser;
+  }
+
+  public boolean verifyEmail (String email) {
+    return userRepository.findUserByEmail(email) == null;
+  }
+
+  public boolean verifyUserName (String username) {
+    return userRepository.findUserByUserName(username) == null;
+  }
+
+  public boolean verifyPhoneNumber (String phoneNumber) {
+    return userRepository.findUserByPhoneNumber(phoneNumber) == null;
   }
 
   public boolean updateUserName (@NotBlank String newUserName, @NotBlank String email) {
-    LogUser logUser = userRepository.findUserByEmail(email);
-    logUser.setUserName(newUserName);
-
+    userRepository.findUserByEmail(email).setUserName(newUserName);
     return true;
   }
 
   public boolean updateUserPhoneNumber (@NotBlank String newPhoneNumber, @NotBlank String email) {
-    LogUser logUser = userRepository.findUserByEmail(email);
-    logUser.setPhoneNumber(newPhoneNumber);
-
+    userRepository.findUserByEmail(email).setPhoneNumber(newPhoneNumber);
     return true;
   }
 
+  public boolean verifyUserLogin(LoginDetails loginDetails) {
+    String email = loginDetails.email();
+    String password = loginDetails.password();
+
+    LogUser logUser = userRepository.findUserByEmail(email);
+    LogUser logUser1 = userRepository.findUserByPhoneNumber(email);
+
+    logUser = logUser == null ? logUser1 : logUser;
+
+    if (logUser != null) {
+      return logUser.getPassword().equals(password)
+              && (logUser.getEmail().equals(email)
+              ||  logUser.getPhoneNumber().equals(email));
+    }
+    return false;
+  }
+
+  public List<LogUser> getAll() {
+    return userRepository.findAll();
+  }
 }
